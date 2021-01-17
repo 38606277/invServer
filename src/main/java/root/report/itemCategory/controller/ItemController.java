5729496e90ee25d56dc0e0c1728bcd3279367a9e
gl36 <381219883@qq.com>
2021-01-17 15:21:52 +0800

    添加新增方法

---
+++ src/main/java/root/report/itemCategory/controller/ItemController.java
@@ -0,0 +1,82 @@
+package root.report.itemCategory.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import root.report.common.RO;
+import root.report.db.DbFactory;
+import root.report.itemCategory.service.ItemService;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/reportServer/item")
+public class ItemController extends RO {
+
+    private static Logger log = Logger.getLogger(ItemController.class);
+
+    @Autowired
+    public ItemService itemService;
+
+    /**
+     * 功能描述: 接收JSON格式参数，往func_dict跟func_dict_out 中插入相关数据
+     */
+    @RequestMapping(value = "/saveItem", produces = "text/plain;charset=UTF-8")
+    public String saveDict(@RequestBody String pJson) throws Exception
+    {
+        SqlSession sqlSession =  DbFactory.Open(DbFactory.FORM);
+        try{
+            sqlSession.getConnection().setAutoCommit(false);
+            JSONObject jsonObject = JSON.parseObject(pJson);
+            String id = this.itemService.saveOrUpdate(sqlSession, jsonObject);
+
+            sqlSession.getConnection().commit();
+            return SuccessMsg("保存成功",id);
+        }catch (Exception ex){
+            sqlSession.getConnection().rollback();
+            ex.printStackTrace();
+            return ExceptionMsg(ex.getMessage());
+        }
+    }
+    /**
+     * 根据类别ID获取数据
+     * */
+    @RequestMapping(value = "/getAllPage", produces = "text/plain;charset=UTF-8")
+    public String getAllPage(@RequestBody String pJson) {
+        try {
+            JSONObject jsonFunc = JSONObject.parseObject(pJson);
+            Map<String,String> map=new HashMap();
+            map.put("startIndex",jsonFunc.getString("startIndex"));
+            map.put("perPage",jsonFunc.getString("perPage"));
+            map.put("item_category_id",jsonFunc.getString("item_category_id"));
+            Map<String,Object> map1 = itemService.getAllPage(map);
+            return SuccessMsg("", map1);
+        } catch (Exception ex){
+            return ExceptionMsg(ex.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/getAllPageByCategoryId", produces = "text/plain;charset=UTF-8")
+    public String getAllPageByCategoryId(@RequestBody String pJson) {
+        try {
+            JSONObject jsonFunc = JSONObject.parseObject(pJson);
+            Map<String,String> map=new HashMap();
+            map.put("item_category_id",jsonFunc.getString("item_category_id"));
+            Map<String,Object> map1 = itemService.getAllPageByCategoryId(map);
+            return SuccessMsg("", map1);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return ExceptionMsg(ex.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/root/report/itemCategory/service/ItemService.java
@@ -0,0 +1,191 @@
+package root.report.itemCategory.service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageRowBounds;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import root.report.db.DbFactory;
+import root.report.mdmDict.service.MdmDictService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ItemService {
+
+    private static Logger log = Logger.getLogger(ItemService.class);
+
+    @Autowired
+    public ItemCategoryService itemCategoryService;
+
+    public Map<String,Object> getAllPage(Map<String,String> map) {
+        Map<String,Object> map1=new HashMap<>();
+
+        try {
+            SqlSession sqlSession = DbFactory.Open(DbFactory.FORM);
+            RowBounds bounds = null;
+            if (map == null) {
+                bounds = RowBounds.DEFAULT;
+            } else {
+                Integer startIndex = Integer.parseInt(map.get("startIndex").toString());
+                Integer perPage = Integer.parseInt(map.get("perPage"));
+                if (startIndex == 1 || startIndex == 0) {
+                    startIndex = 0;
+                } else {
+                    startIndex = (startIndex - 1) * perPage;
+                }
+                bounds = new PageRowBounds(startIndex, perPage);
+            }
+            List<Map<String, Object>> resultList = sqlSession.selectList("mdmItem.getAllPage", map, bounds);
+            Long totalSize = 0L;
+            if (map != null && map.size() != 0) {
+                totalSize = ((PageRowBounds) bounds).getTotal();
+            } else {
+                totalSize = Long.valueOf(resultList.size());
+            }
+
+            map1.put("list", resultList);
+            map1.put("total", totalSize);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return map1;
+    }
+    /**
+     * 功能描述: 根据JSON数据解析 对应数据，生成func_dict记录
+     */
+    public String saveOrUpdate(SqlSession sqlSession,JSONObject jsonObject){
+        Map<String,Object> map  = new HashMap<>();
+        String id="";
+        JSONArray addLine = jsonObject.getJSONArray("lineForm");
+        JSONArray delLine = jsonObject.getJSONArray("lineDelete");
+        Integer newId= sqlSession.selectOne("mdmItem.getMaxId");
+        newId = newId==null?1:newId;
+        for(int i = 0; i < addLine.size(); i++) {
+            Map<String, Object> mapVal = new HashMap<>();
+            JSONObject jsonObjectVal = addLine.getJSONObject(i);
+            if (null != jsonObjectVal.getString("item_id") && !"".equals(jsonObjectVal.getString("item_id")) && jsonObjectVal.getString("item_id").contains("NEW")) {
+                mapVal.put("item_id", newId);
+                mapVal.put("item_category_id", jsonObjectVal.getString("item_category_id"));
+                mapVal.put("item_description", jsonObjectVal.getString("item_description"));
+
+                for (Map.Entry<String, Object> entry : jsonObjectVal.entrySet()) {
+                    System.out.println("key值=" + entry.getKey());
+                    System.out.println("对应key值的value=" + entry.getValue());
+                    if(entry.getKey().equalsIgnoreCase("segment1")) {
+                        mapVal.put("segment1", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment2")) {
+                        mapVal.put("segment2", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment3")) {
+                        mapVal.put("segment3", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment4")) {
+                        mapVal.put("segment4", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment5")) {
+                        mapVal.put("segment5", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment6")) {
+                        mapVal.put("segment6", entry.getValue());
+                    }if(entry.getKey().equalsIgnoreCase("segment7")) {
+                        mapVal.put("segment7", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment8")) {
+                        mapVal.put("segment8", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment9")) {
+                        mapVal.put("segment9", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment10")) {
+                        mapVal.put("segment10", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("attribute1")) {
+                        mapVal.put("attribute1", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("attribute2")) {
+                        mapVal.put("attribute2", entry.getValue());
+                    }
+                }
+                 sqlSession.insert("mdmItem.createMdmItem",mapVal);
+                newId++;
+
+            } else {
+                mapVal.put("item_id", jsonObjectVal.getString("item_id"));
+                id = jsonObjectVal.getString("item_id");
+                for (Map.Entry<String, Object> entry : jsonObjectVal.entrySet()) {
+                    System.out.println("key值=" + entry.getKey());
+                    System.out.println("对应key值的value=" + entry.getValue());
+                    if(entry.getKey().equalsIgnoreCase("segment1")) {
+                        mapVal.put("segment1", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment2")) {
+                        mapVal.put("segment2", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment3")) {
+                        mapVal.put("segment3", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment4")) {
+                        mapVal.put("segment4", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment5")) {
+                        mapVal.put("segment5", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment6")) {
+                        mapVal.put("segment6", entry.getValue());
+                    }if(entry.getKey().equalsIgnoreCase("segment7")) {
+                        mapVal.put("segment7", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment8")) {
+                        mapVal.put("segment8", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment9")) {
+                        mapVal.put("segment9", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("segment10")) {
+                        mapVal.put("segment10", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("attribute1")) {
+                        mapVal.put("attribute1", entry.getValue());
+                    }
+                    if(entry.getKey().equalsIgnoreCase("attribute2")) {
+                        mapVal.put("attribute2", entry.getValue());
+                    }
+                }
+                sqlSession.insert("mdmItem.updateMdmItem", mapVal);
+            }
+        }
+        if(delLine.size()>0) {
+            /*for (int i = 0; i < delLine.size(); i++) {
+                Map<String, Object> dmap = new HashMap<>();
+                JSONObject jsonObjectVal = addLine.getJSONObject(i);
+                if(!jsonObjectVal.getString("row_number").contains("NEW")) {
+                    dmap.put("category_id", jsonObjectVal.getString("category_id"));
+                    dmap.put("row_number", jsonObjectVal.getString("row_number"));
+                    sqlSession.delete("mdmItem.deleteItemCategorySegmentByID", dmap);
+                }
+            }*/
+        }
+        return id;
+    }
+
+
+    public Map getAllPageByCategoryId(Map<String, String> map) {
+        Map resmap =new HashMap();
+        List<Map> list= DbFactory.Open(DbFactory.FORM).selectList("mdmItem.getAllPageByCategoryId",map);
+        Map paramMap= new HashMap();
+        paramMap.put("category_id",map.get("item_category_id"));
+        Map maps=itemCategoryService.getItemCategoryById(paramMap);
+        resmap.put("mainForm",maps);
+        resmap.put("lineForm",list);
+        return resmap;
+    }
+}
