865900db0d0aa29bcf354bc082432ee1210be8c8
gl36 <381219883@qq.com>
2021-01-27 09:53:23 +0800

    添加库存现有量查询

---
+++ src/main/java/root/report/inventory/controller/InventoryController.java
@@ -0,0 +1,47 @@
+package root.report.inventory.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import root.report.common.RO;
+import root.report.db.DbFactory;
+import root.report.inventory.service.InventoryService;
+import root.report.itemCategory.service.ItemCategoryService;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/reportServer/inventory")
+public class InventoryController extends RO {
+
+    private static Logger log = Logger.getLogger(InventoryController.class);
+
+    @Autowired
+    public InventoryService inventoryService;
+
+    @RequestMapping(value = "/getAllPage", produces = "text/plain;charset=UTF-8")
+    public String getAllPage(@RequestBody String pJson) {
+        try {
+            JSONObject jsonFunc = JSONObject.parseObject(pJson);
+            Map<String,String> map=new HashMap();
+            map.put("startIndex",jsonFunc.getString("startIndex"));
+            map.put("perPage",jsonFunc.getString("perPage"));
+            map.put("item_description",jsonFunc.getString("item_description"));
+            Map<String,Object> map1 = inventoryService.getAllPage(map);
+            return SuccessMsg("", map1);
+        } catch (Exception ex){
+            return ExceptionMsg(ex.getMessage());
+        }
+    }
+
+
+}
---
+++ src/main/java/root/report/inventory/service/InventoryService.java
@@ -0,0 +1,60 @@
+package root.report.inventory.service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageRowBounds;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import root.report.db.DbFactory;
+import root.report.mdmDict.service.MdmDictService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class InventoryService {
+
+    private static Logger log = Logger.getLogger(InventoryService.class);
+
+
+    public Map<String,Object> getAllPage(Map<String,String> map) {
+        Map<String,Object> map1=new HashMap<>();
+
+        try {
+            SqlSession sqlSession = DbFactory.Open(DbFactory.FORM);
+            RowBounds bounds = null;
+            if (map == null) {
+                bounds = RowBounds.DEFAULT;
+            } else {
+                Integer startIndex = Integer.parseInt(map.get("startIndex").toString());
+                Integer perPage = Integer.parseInt(map.get("perPage"));
+                if (startIndex == 1 || startIndex == 0) {
+                    startIndex = 0;
+                } else {
+                    startIndex = (startIndex - 1) * perPage;
+                }
+                bounds = new PageRowBounds(startIndex, perPage);
+            }
+            List<Map<String, Object>> resultList = sqlSession.selectList("inventory.getAllPage", map, bounds);
+            Long totalSize = 0L;
+            if (map != null && map.size() != 0) {
+                totalSize = ((PageRowBounds) bounds).getTotal();
+            } else {
+                totalSize = Long.valueOf(resultList.size());
+            }
+
+            map1.put("list", resultList);
+            map1.put("total", totalSize);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return map1;
+    }
+
+}
